f4de0bdea5e5efe382ae6956b14726c23a7dd9e8
Ronaldo Meneguite <ronaldo@fireguard.com.br>
2019-12-21 10:57:56 -0300

    Update install script

--- install.sh
+++ install.sh
@@ -13,60 +13,61 @@ brew update
 # http://formulae.brew.sh/
 # Or brew list 'package-name'
 
+# Install Bash 5 using homebrew
+brew install bash
+echo /usr/local/bin/bash >> /etc/shells
+chsh -s /usr/local/bin/bash
+echo "BASH_VERSION: ${BASH_VERSION}"
+
+# Install ZSH terminal
+#brew install zsh
+#sudo -s 'echo /usr/local/bin/zsh >> /etc/shells' && chsh -s /usr/local/bin/zsh
+#sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+# Get theme
+# https://github.com/mbadolato/iTerm2-Color-Schemes
+# https://raw.githubusercontent.com/mbadolato/iTerm2-Color-Schemes/master/xfce4terminal/colorschemes/JetBrains%20Darcula.theme
+# TODO: Finish Automated Instalation ZSH themes
+
+
 # Development
-brew install php
+#brew install php
 # brew install php@7.1
 # brew install php-cs-fixer
 # brew install php-code-sniffer
-brew install composer
+#brew install composer
 brew install nvm
 nvm install node
 brew install git
 #brew install git-lfs
 brew install git-flow
 brew install git-extras
-brew install angular-cli
+#brew install angular-cli
 
 # Utils
 brew install curl
 brew install wget
-brew install rsync
+#brew install rsync
 brew install coreutils
 
 # Install GnuPG to enable PGP-signing commits.
-brew install gnupg
+#brew install gnupg
 
 # Install more recent versions of some macOS tools.
-brew install vim --with-override-system-vi
+brew install vim
 
 # Install some other useful utilities like `sponge`.
-brew install moreutils
+#brew install moreutils
 
 # Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
 brew install findutils
 
 # Install GNU `sed`, overwriting the built-in `sed`.
-brew install gnu-sed --with-default-names
+brew install gnu-sed
 
 brew install openssl
 brew install libyaml
 brew install grep
 brew install tree
-
-# Install Bash 4 using homebrew
-# brew install bash
-# echo /usr/local/bin/bash >> /etc/shells
-# chsh -s /usr/local/bin/bash
-# echo "BASH_VERSION: ${BASH_VERSION}"
-
-# Install ZSH terminal
-brew install zsh
-sudo -s 'echo /usr/local/bin/zsh >> /etc/shells' && chsh -s /usr/local/bin/zsh
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-# Get theme
-# https://github.com/mbadolato/iTerm2-Color-Schemes
-# https://raw.githubusercontent.com/mbadolato/iTerm2-Color-Schemes/master/xfce4terminal/colorschemes/JetBrains%20Darcula.theme
-# TODO: Finish Automated Instalation ZSH themes
 
 ###############################################################################
 # Homebrew Cask                                                               #
@@ -75,10 +76,6 @@ sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/i
 # Search
 # https://caskroom.github.io/search
 
-# homebrew-cask
-brew tap caskroom/cask
-brew tap caskroom/versions
-
 # browser
 brew cask install google-chrome
 brew cask install firefox
@@ -86,24 +83,23 @@ brew cask install firefox
 # development
 brew cask install iterm2
 brew cask install docker
-brew cask install phpstorm
+#brew cask install phpstorm
 brew cask install visual-studio-code
-#brew cask install sublime-text
+brew cask install sublime-text
 brew cask install sourcetree
-#brew cask install rdm # Redis Desktop Manager
 brew cask install postman
 brew cask install mysqlworkbench
 brew cask install sequel-pro
-#brew cask install sqlitestudio
-#brew cask install mongodb-compass
+brew cask install sqlitestudio
+brew cask install mongodb-compass
 
 # other
 brew cask install slack
 brew cask install skype
 brew cask install vlc
 brew cask install the-unarchiver
-#brew cask install teamviewer
-#brew cask install boxcryptor
+brew cask install teamviewer
+brew cask install boxcryptor
 #brew cask install spotify
 brew cask install google-backup-and-sync
 #brew cask install whatsapp
